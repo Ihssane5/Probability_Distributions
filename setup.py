b6c10b899e13b0d455209f70abc748631d42c59a
Ihssane5 <ihssanenedjaoui5@gmail.com>
2024-08-04 16:18:07 +0100

    Pushing the setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'p-distributions-2024',
+      version = '1',
+      description = 'Package with probability distributions',
+      packages=['p-distributions-2024'],
+      zip_safe=False)
